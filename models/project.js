5c3bff02bdc5435b3102db834bd7c02137839230
Yev <yevyork@gmail.com>
2019-12-31 16:58:49 -0500

    formatting

--- models/project.js
+++ models/project.js
@@ -1,24 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-  const Project = sequelize.define('Project', {
-    title: DataTypes.STRING,
-    imageUrl: DataTypes.STRING,
-    description: DataTypes.STRING,
-    githubUrl: DataTypes.STRING,
-    deployedUrl: DataTypes.STRING,
-    userId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'User',
-        key: 'id',
-        as: 'userId',
+  const Project = sequelize.define(
+    "Project",
+    {
+      title: DataTypes.STRING,
+      imageUrl: DataTypes.STRING,
+      description: DataTypes.STRING,
+      githubUrl: DataTypes.STRING,
+      deployedUrl: DataTypes.STRING,
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "User",
+          key: "id",
+          as: "userId"
+        }
       }
-    }
-  }, {});
-  Project.associate = function (models) {
+    },
+    {}
+  );
+  Project.associate = function(models) {
     Project.belongsTo(models.User, {
-      foreignKey: 'userId',
-      onDelete: 'CASCADE'
-    })
+      foreignKey: "userId",
+      onDelete: "CASCADE"
+    });
   };
   return Project;
 };
--- models/user.js
+++ models/user.js
@@ -1,13 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
+  const User = sequelize.define(
+    "User",
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING
+    },
+    {}
+  );
   User.associate = function(models) {
     User.hasMany(models.Project, {
-      foreignKey: 'userId'
-    })
+      foreignKey: "userId"
+    });
   };
   return User;
 };
